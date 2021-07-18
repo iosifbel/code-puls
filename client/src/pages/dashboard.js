88a3bbf25cb74dce8c5d0f2fd8760ac0d7c1c471
iosifbel <iosifbel@users.noreply.github.com>
2021-07-18 06:02:37 +0300

    dashboard design structure added

--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -2,30 +2,79 @@ import styled from "styled-components";
 import React, { useContext } from "react";
 import { AppContext } from "../context/context";
 import { AuthContext } from "../context/AuthContext";
+import { Card } from "../components/defaultComponents";
+import { Form } from "react-bootstrap";
+import theme from "../Assets/theme";
 
 function Dashboard(props) {
-  const auth = useContext(AuthContext);
-  const { authState } = auth;
+  const user = useContext(AuthContext).authState.userInfo;
   const { setShowHeader, setShowNavbar } = React.useContext(AppContext);
   setShowHeader(true);
   setShowNavbar(true);
   return (
     <Wrapper>
-      {" "}
-      <div>
-        <h1>Panou de lucru</h1>
-        <h2>{authState.userInfo ? authState.userInfo.tip : "none"}</h2>
+      <GreetingsCard>
+        <h1>Buna {user.nume},</h1>
+        <h2>Cum te simți astăzi?</h2>
+      </GreetingsCard>
+      <div className="graphCardsContainer">
+        <GraphCard></GraphCard>
+        <GraphInputCard className="align-items-center justify-content-center">
+          <Form.Group>
+            <Form.Label>
+              Alege {user.tip === "student" ? "materia" : "grupa"}
+            </Form.Label>
+            <Form.Control as="select" className="Forminput"></Form.Control>
+          </Form.Group>
+        </GraphInputCard>
       </div>
     </Wrapper>
   );
 }
+const GraphInputCard = styled(Card)`
+  flex-grow: 1;
+  // .Forminput {
+  //   width: 5rem;
+  // }
+  padding: 5rem;
+`;
+const GraphCard = styled(Card)`
+  flex-grow: 2;
+  margin-right: 1rem;
+`;
+const GreetingsCard = styled(Card)`
+  width: 100%;
+  margin-bottom: 1rem;
+  h1 {
+    font-weight: normal;
+    font-size: 1.5rem;
+    margin-bottom: 1rem;
+  }
+
+  h2 {
+    font-weight: normal;
+    font-size: 1rem;
+  }
+`;
 
 const Wrapper = styled.div`
   display: flex;
-  height: 90vh;
+  flex-direction: column;
+  margin-left: 25vw;
+  margin-top: 10vh;
+  margin-right: 10vw;
+  margin-bottom: 10vh;
+  height: 63vh;
   align-items: center;
   justify-content: center;
-  font-size: 3rem;
+  font-size: 1rem;
+  color: ${theme.mainBlack};
+  .graphCardsContainer {
+    flex-grow: 1;
+    width: 100%;
+    display: flex;
+    height: auto;
+  }
 `;
 
 export default Dashboard;
